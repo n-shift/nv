b835369fa5258d6ca0e96390362d273af4af29a7
shift-d <me@shift-d.tk>
2021-08-25 20:54:22 +0300

    feat: nvimtree config

---
+++ lua/config/nvimtree.lua
@@ -0,0 +1,8 @@
+vim.g.nvim_tree_side = "right"
+vim.g.nvim_tree_ignore = { ".git", "node_modules", "target" }
+vim.g.nvim_tree_auto_open = 1
+vim.g.nvim_tree_auto_resize = 0
+vim.g.nvim_tree_add_trailing = 1
+vim.g.nvim_tree_update_cwd = 1
+vim.g.nvim_tree_show_icons = { git = 1, folders = 1, files = 1, folder_arrows = 1 }
+vim.g.nvim_tree_special_files = { ["README.md"] = 1 }
