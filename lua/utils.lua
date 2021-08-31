0090d1724f32f3f8d8ba8f199c3249566e27f072
shift-d <me@shift-d.tk>
2021-08-31 12:05:16 +0300

    refactor: move vimscript into lua file

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,5 +1,3 @@
-vim.cmd("source utils.vim")
-
 _G.prequire = function(plugin)
     local present, plug = pcall(require, plugin)
     if present then
@@ -8,4 +6,22 @@ _G.prequire = function(plugin)
     local errmsg = string.format("Could not load %s", plugin)
     print(errmsg)
 end
+
+vim.cmd [[
+" functions that are unnecessary to write in lua
+
+" open github link under cursor
+function Github()
+    let @g = ""
+    normal "gyi"
+    if @g != ""
+        if has('win32') || has('win64')
+            call system("start " . "https://github.com/" . @g)
+        else
+            call system("open " . "https://github.com/" . @g)
+        endif
+    endif
+    let @g = ""
+endfunction
+]]
 
