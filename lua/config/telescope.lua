4e8ba5d0dcb463a7eee54852768bcc5a08bc24c9
shift-d <me@shift-d.tk>
2021-09-10 17:52:32 +0300

    chore(plugins): default initial mode for telescope

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -7,7 +7,6 @@ telescope.setup({
     defaults = {
         prompt_prefix = "τ ",
         entry_prefix = "○ ",
-        initial_mode = "normal",
         set_env = { ["COLORTERM"] = "truecolor" },
     },
 })
